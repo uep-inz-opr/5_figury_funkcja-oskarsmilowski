8ffd63d7bfb8a15efda3189d007be5636de6844a
Oskar <69815@student.ue.poznan.pl>
2022-04-10 14:15:44 +0200

    cala funkcjonalnosc ale zly wynik

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import math
 
-def pole_kola(r):
-    return math.pi*r**2
-
-def pole_prostokata(a,b):
-    return a*b
-
-def pole_trojkata(a,b,c):
-    p = (a+b+c)/2
-    return math.sqrt(p*(p-a)*(p-b)*(p-c))
-
-
+def licz_pole(*args):
+    if len(args) == 1:
+        return math.pi*(args[0]**2)
+    elif len(args) == 2:
+        return args[0]*args[1]
+    elif len(args) == 3:
+        p = (args[0] + args[1] + args[2])/2
+        return math.sqrt(p * (p-args[0]) * (p-args[1]) * (p-args[2]))
+    else:
+        return "Błąd"     
 
 def main():
     liczba_figur = int(input())
     figury = []
-    for i in range(liczba_figur):
+    for _ in range(liczba_figur):
         figura = input()
         figury.append(figura)
+    wynik = 0.0
 
-    print(figury)
+    for figura in figury:
+        figura = figura.split(' ')
+        figura = [float(x) for x in figura]
+        pole = licz_pole(*figura)
+        if pole == "Błąd":
+            print("Błąd: można podać maksymalnie 3 liczby")
+            return
+        else:
+            wynik += pole
+        
+    wynik = round(wynik, 2)
+    print(wynik)
+    
 
 if __name__ == '__main__':
     main()
