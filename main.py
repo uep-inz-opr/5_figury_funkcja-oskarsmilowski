cd311a7f982a5161e2d6148da78da35f3e2bd5f2
Oskar <69815@student.ue.poznan.pl>
2022-04-10 14:26:17 +0200

    do sprawdzarki

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import math
 
 def licz_pole(*args):
     if len(args) == 1:
-        return math.pi*(args[0]**2)
+        return math.pi*pow(args[0], 2)
     elif len(args) == 2:
         return args[0]*args[1]
     elif len(args) == 3:
         p = (args[0] + args[1] + args[2])/2
-        return math.sqrt(p * (p-args[0]) * (p-args[1]) * (p-args[2]))
+        to_sqrt = p * (p-args[0]) * (p-args[1]) * (p-args[2])
+        return math.sqrt(to_sqrt)
     else:
         return "Błąd"     
 
