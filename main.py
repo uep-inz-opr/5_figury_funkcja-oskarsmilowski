4a603806d6e851a6e6c13fa2af1ea2195ddc9ec2
Oskar <69815@student.ue.poznan.pl>
2022-04-10 13:04:04 +0200

    dodanie funkcji do liczenia pol

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import math
+
+def pole_kola(r):
+    return math.pi*r**2
+
+def pole_prostokata(a,b):
+    return a*b
+
+def pole_trojkata(a,b,c):
+    p = (a+b+c)/2
+    return math.sqrt(p*(p-a)*(p-b)*(p-c))
+
+
+
 def main():
-    print('hello')
+    liczba_figur = int(input())
+    figury = []
+    for i in range(liczba_figur):
+        figura = input()
+        figury.append(figura)
+
+    print(figury)
 
 if __name__ == '__main__':
     main()
